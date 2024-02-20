352aeaaa52788d4469a7779dcbe56dc8876a388a
Jéssica Guebur <146867683+Jessica-Guebur@users.noreply.github.com>
2024-02-20 13:40:29 -0300

    Add files via upload

---
+++ src/scripts/menu-database.js
@@ -0,0 +1,34 @@
+const menuFoods = [
+    {
+        name: `Hamburguer`,
+        price: 15,
+        img: "./src/assets/imgs/hamburguer.jpg",
+    },
+    {
+        name: `Churras`,
+        price: 34.9,
+        img: "./src/assets/imgs/churras.jpg",
+    },
+    {
+        name: `Sushi`,
+        price: 40,
+        img: "./src/assets/imgs/sushi.jpg",
+    },
+    {
+        name: `Salada`,
+        price: 12,
+        img: "./src/assets/imgs/salada.jpg",
+    },
+    {
+        name: `Salada de Frutas`,
+        price: 13.9,
+        img: "./src/assets/imgs/salada-frutas.jpg",
+    },
+    {
+        name: `Bolo de Morango`,
+        price: 26.9,
+        img: "./src/assets/imgs/bolo-morango.jpg",
+    },
+
+]
+
